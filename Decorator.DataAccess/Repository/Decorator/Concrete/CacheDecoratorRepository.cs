073085175c0ca8e0e86cd3d05471fd59257fff54
AYSE PARLA YERLI <AYSE.YERLI@lcwaikiki.com>
2023-12-19 23:53:43 +0300

    edit#

--- Decorator.DataAccess/Repository/Decorator/Concrete/CacheDecoratorRepository.cs
+++ Decorator.DataAccess/Repository/Decorator/Concrete/CacheDecoratorRepository.cs
@@ -1,8 +1,6 @@
 ﻿using Decorator.Data.Models;
 using Microsoft.Extensions.Caching.Memory;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Decorator.DataAccess.Repository.Decorator.Concrete
@@ -11,12 +9,10 @@ namespace Decorator.DataAccess.Repository.Decorator.Concrete
     {
         private const string cacheName = "products";
         private readonly IMemoryCache _memoryCache;
-
         public CacheDecoratorRepository(IProductRepository productRepository, IMemoryCache memoryCache) : base(productRepository)
         {
             _memoryCache = memoryCache;
         }
-
         public override async Task<List<Products>> GetAll()
         {
             if (_memoryCache.TryGetValue(cacheName, out List<Products> cacheProducts))
@@ -28,36 +24,27 @@ namespace Decorator.DataAccess.Repository.Decorator.Concrete
             _memoryCache.Set(cacheName, allProducts);
             return allProducts;
         }
-
         public override async Task<Products> CreateProduct(Products product)
         {
             await base.CreateProduct(product);
-            await CacheUpdateData(product);
-
+            await CacheUpdateData();
             return product;
         }
         public override async Task UpdateProduct(Products model)
         {
             await base.UpdateProduct(model);
-            await CacheUpdateData(model);
+            await CacheUpdateData();
         }
         public override async Task DeleteProduct(int id)
         {
             await base.DeleteProduct(id);
             await CacheUpdateData();
         }
-        private async Task CacheUpdateData(Products product = null)
+        private async Task CacheUpdateData()
         {
-            var allProducts = await base.GetAll();
-            _memoryCache.Set(cacheName, allProducts);
-
-            if (product != null)
-            {
-                var newKey = Guid.NewGuid().ToString();// Ya da Guid.NewGuid().ToString() kullanabilirsiniz.
-                _memoryCache.Set(newKey, product); // Yeni anahtar ile ürünü de ekleyin
-            }
+            _memoryCache.Remove(cacheName);
+            _memoryCache.Set(cacheName, await base.GetAll());
         }
     }
-
 }
 
